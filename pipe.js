c47d9febf6f632e2184d3d4417f9fd9f140d5b24
Paulo Andre de Oliveira Carneiro <paulo_carneiro@usp.br>
2018-08-20 16:53:36 -0300

    adding first version of Flappy Bird

---
+++ pipe.js
@@ -0,0 +1,35 @@
+function Pipe(){
+	this.hole = 100;
+	this.top = (random() * 550) + 1;
+	this.bottom = height - this.top - this.hole;
+	this.x = width;
+	this.w = 20;
+	this.speed = 2;
+	this.highlight = false;
+
+	this.show = function(){
+		fill(255);
+		if(this.highlight == true){
+			fill(255, 0, 0);
+		}
+		rect(this.x, 0, this.w, this.top); 
+		rect(this.x, height - this.bottom, this.w, this.bottom);
+	}
+	this.update = function(){
+		this.x -= this.speed;
+	}
+	this.offscreen = function(){
+		if(this.x < -this.w){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	this.hits = function(bird){
+		if((bird.y < this.top || bird.y > height - this.bottom) && (bird.x > this.x && bird.x < this.x + this.w)){
+			this.highlight = true;			
+			return true;
+		}
+	}
+}
---
+++ sketch.js
@@ -0,0 +1,41 @@
+var bird;
+var pipes = [];
+var placar = 0;
+function setup() {
+  createCanvas(400, 600);
+  bird = new Bird();
+  pipes.push(new Pipe());
+}
+
+function draw() {
+  background(0);
+  bird.update();
+  bird.show();
+  document.getElementById("placar").innerHTML = placar;
+  if(frameCount % 100 == 0){
+  	pipes.push(new Pipe());
+  }
+
+  for(var i = pipes.length -1 ; i >= 0 ; i--){
+  	pipes[i].show();
+  	pipes[i].update();
+
+  	if(pipes[i].hits(bird)){
+  		placar = 0;
+  		console.log("HIT");
+  	}
+
+  	if(pipes[i].offscreen()){
+  		(pipes[i].highlight == false)
+  			placar += 1;
+  		pipes.splice(i,1);
+  	}
+  }
+
+}
+
+function keyPressed(){
+	if(key == ' '){
+		bird.up();
+	}
+}
