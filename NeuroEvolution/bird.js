e47b0d34b10c193c926589372243f0b1d6df65ac
Paulo Andre de Oliveira Carneiro <paulo_carneiro@usp.br>
2018-10-25 21:57:01 -0200

    iniciando projeto GA

---
+++ NeuroEvolution/bird.js
@@ -0,0 +1,78 @@
+function Bird(brain) {
+	this.y = height/2;
+	this.x = 40;
+
+	this.score = 0;
+	this.fitness = 0;
+
+
+	this.gravity = 0.6	;
+	this.velocity = 0;
+	this.lift = -15;
+	if(brain)
+		this.brain = brain.copy();
+	else
+		this.brain = new NeuralNetwork(4,4,2);
+	this.think = function(pipes){
+		var closest = null;
+		var closestD = Infinity;
+		for(var i = 0; i < pipes.length; i++){
+			var aux = (pipes[i].x - this.x);
+			if(aux < closestD && aux > 0){
+				closest = pipes[i];
+				closestD = aux;
+			} 
+		}
+
+
+		this.inputs = [];
+		this.inputs[0] = this.y / height;
+		this.inputs[1] = closest.top / height;
+		this.inputs[2] = closest.bottom / height;
+		this.inputs[3] = closest.x / width;
+
+		this.output = this.brain.predict(this.inputs);
+		if(this.output[0] > this.output[1]){
+			this.up();
+		}
+	}
+
+
+	this.mutate = function(){
+		console.log(this)
+		this.brain.mutate(0.1);
+	}
+
+	this.getFitness = function(){
+		return this.fitness;
+	}
+
+	this.update = function(){
+
+		this.score++;
+
+
+		this.velocity += this.gravity;
+		this.velocity *= 0.9;
+		this.y += this.velocity;
+
+		if(this.y > height){
+			this.y = height;
+			this.velocity = 0;
+		}
+		if(this.y < 0){
+			this.y = 0;
+			this.velocity = 0;
+		}
+	}
+
+	this.show = function(){
+		stroke(255);
+		fill(255, 50);
+		ellipse(this.x, this.y, 32, 32);
+	}
+
+	this.up = function(){
+		this.velocity += this.lift;
+	}
+}
---
+++ NeuroEvolution/ga.js
@@ -0,0 +1,38 @@
+function nextGeneration(){
+
+	calculateFitness();
+	for(var i=0; i< total; i++){
+		bird[i] = pickOne();	
+	}
+	savedBirds = []; //limpando as geracoes passadas
+}
+
+//Aqui escolhe os melhores passaros
+function pickOne(){
+	var index = 0
+	var r = random(1)
+
+	while(r > 0){
+		r = r - savedBirds[index].getFitness();
+		index++;
+	}
+	index--;
+
+	var b = savedBirds[index];
+	var child = new Bird(b.brain);
+	//child.mutate();
+	return child;
+}
+
+
+
+function calculateFitness(){
+	var sum = 0;
+	for(var b of savedBirds){
+		sum += b.score;
+	}
+
+	for(var b of savedBirds){
+		b.fitness += b.score/sum;
+	}
+}
